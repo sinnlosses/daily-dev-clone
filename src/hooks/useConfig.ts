4e36e104e7fb1712b8e99bdc2db16f99dd39c37b
fuji <fujiyoshiyuki@gmail.com>
2023-12-10 19:40:38 +0900

    config を freeze

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -1,7 +1,10 @@
 export function useConfig() {
-  return {
+  const config = {
     assets: {
       headerLogoPath: process.env.NEXT_PUBLIC_HEADER_LOGO_PATH || "/dolphin.png",
     },
   }
+  Object.freeze(config)
+
+  return config
 }
