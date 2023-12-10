8633f97dc0b162fa80b84ae845fa46a85010e921
fuji <fujiyoshiyuki@gmail.com>
2023-12-10 15:44:48 +0900

    ヘッダーのメディアクエリ対応

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,15 +1,19 @@
 "use client"
 import { HamburgerIcon } from "@/components/ui/icons"
+import { useMediaContext } from "@/providers/media"
 import Image from "next/image"
 import Link from "next/link"
 import styles from "./header.module.css"
 
 export function Header() {
+  const { isMobile } = useMediaContext()
   return (
     <header className={styles.header}>
-      <button className={styles.button}>
-        <HamburgerIcon />
-      </button>
+      {isMobile && (
+        <button className={styles.button}>
+          <HamburgerIcon />
+        </button>
+      )}
       <Link href={"/"} className={styles.link}>
         <Image src={"/dolphin.png"} width={100} height={30} alt="logo" priority />
       </Link>
--- src/providers/media/use-media.ts
+++ src/providers/media/use-media.ts
@@ -2,8 +2,10 @@ import { useMediaQuery } from "usehooks-ts"
 
 export type UseMediaReturn = {
   isDesktop: boolean
+  isMobile: boolean
 }
 export function useMedia(breakpoint: number): UseMediaReturn {
-  const isDesktop = useMediaQuery(`min-width: ${breakpoint}px`)
-  return { isDesktop }
+  const isDesktop = useMediaQuery(`(min-width: ${breakpoint}px)`)
+  const isMobile = !isDesktop
+  return { isDesktop, isMobile }
 }
