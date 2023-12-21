db991b6868b4da1eed97ca10aac28ceaacc78f09
fuji <fujiyoshiyuki@gmail.com>
2023-12-21 22:48:26 +0900

    歯車アイコンの調整

--- src/components/layouts/components/sidebar/presentational-sidebar.tsx
+++ src/components/layouts/components/sidebar/presentational-sidebar.tsx
@@ -1,8 +1,10 @@
 import { UseSidebarReturnType } from "@/components/layouts/components/sidebar/hooks/use-sidebar"
 import { HStack } from "@/components/ui/hstack"
-import { ChevronLeft, GearIcon } from "@/components/ui/icons"
+import { ChevronLeft, ChevronRight, GearIcon } from "@/components/ui/icons"
+import { StackItem } from "@/components/ui/stack-item"
 import { Text } from "@/components/ui/text"
 import { VStack } from "@/components/ui/vstack"
+import clsx from "clsx"
 import styles from "./sidebar.module.css"
 
 type PresentationalSidebarProps = UseSidebarReturnType
@@ -13,50 +15,27 @@ export const PresentationalSidebar = ({
   showModal,
 }: PresentationalSidebarProps) => {
   return (
-    <aside className={styles.sidebar}>
-      {isExpanded ? (
-        <>
-          <button onClick={() => setIsExpanded(false)} className={styles.closeButton}>
-            <ChevronLeft />
-          </button>
-          <VStack gap="l" className={styles.contents}>
-            {Array.from(Array(10)).map((_, index) => {
-              return (
-                <VStack key={index} gap="s">
-                  <p className={styles.category}>Manage</p>
-                  <button className={styles.item} onClick={showModal}>
-                    <HStack alignItems="center" paddingLeft="s" gap="s" className={styles.item}>
-                      <GearIcon />
-                      <Text>Customize</Text>
-                    </HStack>
-                  </button>
-                </VStack>
-              )
-            })}
-          </VStack>
-        </>
-      ) : (
-        <>
-          <button onClick={() => setIsExpanded(true)} className={styles.closeButton}>
-            <ChevronLeft />
-          </button>
-          <VStack gap="l" className={styles.contents}>
-            {Array.from(Array(1)).map((_, index) => {
-              return (
-                <VStack key={index} gap="s">
-                  <p className={styles.category}>Manage</p>
-                  <button className={styles.item} onClick={showModal}>
-                    <HStack alignItems="center" paddingLeft="s" gap="s" className={styles.item}>
-                      <GearIcon />
-                      <Text>Customize</Text>
-                    </HStack>
-                  </button>
-                </VStack>
-              )
-            })}
-          </VStack>
-        </>
-      )}
+    <aside className={clsx(styles.sidebar, isExpanded ? styles.sidebarWide : styles.sidebarNarrow)}>
+      <button onClick={() => setIsExpanded((prev) => !prev)} className={styles.closeButton}>
+        {isExpanded ? <ChevronLeft /> : <ChevronRight />}
+      </button>
+      <VStack gap="l" className={styles.contents}>
+        {Array.from(Array(10)).map((_, index) => {
+          return (
+            <VStack key={index} gap="s">
+              <p className={clsx(styles.category, !isExpanded && styles.hidden)}>Manage</p>
+              <button className={styles.item} onClick={showModal}>
+                <HStack alignItems="center" paddingLeft="s" gap="s">
+                  <StackItem noShrink>
+                    <GearIcon />
+                  </StackItem>
+                  <Text className={clsx(!isExpanded && styles.hidden)}>Customize</Text>
+                </HStack>
+              </button>
+            </VStack>
+          )
+        })}
+      </VStack>
     </aside>
   )
 }
